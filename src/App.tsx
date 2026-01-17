d9bb2fd96d3c5c42be95654b81ff2912c1e19448
Mahi <mahi@example.com>
2026-01-17 12:14:48 +0530

    fix: Add basename to BrowserRouter for GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/shreetama-website/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/shop" element={<Shop />} />
